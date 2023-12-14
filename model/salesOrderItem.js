bf76491f74fe597456b433b97e48c69edb6db870
ManjhiShubham <112389058+ManjhiShubham@users.noreply.github.com>
2023-12-15 00:34:26 +0530

    changes

---
+++ model/salesOrderItem.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+// Define the schema
+const salesOrderItemSchema = new mongoose.Schema({
+  _id: String,
+  master_sku_name: String,
+  id: Number,
+  sales_order_id: Number,
+  master_sku_id: Number,
+  created_by_id: Number,
+  last_updated_by_id: Number,
+  product_name: String,
+  quantity: Number,
+  rate: Number,
+  mrp: Number,
+  gst_rate: Number,
+  gst_amount: Number,
+  cgst_rate: Number,
+  cgst_amount: Number,
+  igst_rate: Number,
+  igst_amount: Number,
+  net_amount: Number,
+  total_amount: Number,
+  total_tax_amount: Number,
+  created_at: Date,
+  updated_at: Date,
+  unit_of_measurement: String,
+  discount_rate: Number,
+  free_quantity: Number
+});
+
+// Create the model
+const SalesOrderItem = mongoose.model('sales_order_items', salesOrderItemSchema, 'sales_order_items');
+
+// Export the model
+module.exports = SalesOrderItem;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var Order = require("../model/orders")
-const SalesOrder = require("../model/salesOrders")
+const SalesOrder = require("../model/salesOrder")
+const SalesorderItems = require("../model/salesOrderItem");
 
 var Assistant = require('../chatbot/index');
 const assistant = new Assistant();
@@ -11,12 +12,12 @@ assistant.initializeChat();
 /* GET home page. */
 router.get('/', async function(req, res, next) {
     var query = req.query;
-    const orderId = '657b35d61a8e852d14918718';
-    // const order = await SalesOrder.find({id: parseInt(23727)});
-    // console.log(order);
+    const orderId = '657b38a01a8e852d14928d89';
+    const order = await SalesorderItems.find({id: parseInt(23727)});
+    console.log(order);
 
-  const result = await assistant.sendMessage(query.msg);
-  res.json(result);
+  // const result = await assistant.sendMessage(query.msg);
+  res.json(order);
 });
 
 module.exports = router;
