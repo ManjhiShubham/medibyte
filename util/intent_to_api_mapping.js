8238d93d9f947898ceff568e9bca34f084e41fab
Kartikeya05S <73102048+Kartikeya05S@users.noreply.github.com>
2023-12-15 06:25:13 +0530

    intent to api mapping created

---
+++ util/intent_to_api_mapping.js
@@ -0,0 +1,13 @@
+const mapping = {
+  Order: {
+    get_active_orders: {
+      api: "/orders/sales_order"
+    },
+    get_last_n_orders: {
+      api: "/orders/sales_order"
+    },
+    get_order_detail_by_id: {
+      api: "/orders/sales_order"
+    }
+  }
+};
